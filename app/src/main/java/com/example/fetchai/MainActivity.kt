8048ecdbe3107065e07eb09190fdaf3733fe9f9f
Neirotciv <tanacetifolia@gmail.com>
2024-03-28 21:45:16 +0100

    feat(libert.ai): fetch api with message

--- app/src/main/java/com/example/fetchai/MainActivity.kt
+++ app/src/main/java/com/example/fetchai/MainActivity.kt
@@ -16,7 +16,16 @@ import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import com.example.fetchai.ui.theme.FetchAITheme
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.GlobalScope
+import kotlinx.coroutines.launch
+import java.io.BufferedReader
+import java.io.DataOutputStream
+import java.io.InputStreamReader
+import java.io.OutputStreamWriter
 import java.lang.reflect.Field
+import java.net.HttpURLConnection
+import java.net.URL
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -28,17 +37,58 @@ class MainActivity : ComponentActivity() {
 
         validateButton.setOnClickListener {
             val message = demandMessage.text.toString()
-            isEmptyEditText(demandMessage)
+            if(!isEmptyEditText(demandMessage)) {
+                makeHttpRequest(message)
+            }
         }
     }
 
-    private fun isEmptyEditText(demandMessage: EditText) {
+    private fun isEmptyEditText(demandMessage: EditText): Boolean {
         val errorText = findViewById<TextView>(R.id.errorTextView)
 
         if (demandMessage.text.toString().isEmpty()) {
             errorText.visibility = TextView.VISIBLE
-        } else {
-            errorText.visibility = TextView.GONE
+            return true
+        }
+        errorText.visibility = TextView.GONE
+        return false
+    }
+
+    private fun makeHttpRequest(text: String) {
+        GlobalScope.launch(Dispatchers.IO) {
+            try {
+                val urlString = "https://curated.aleph.cloud/vm/a8b6d895cfe757d4bc5db9ba30675b5031fe3189a99a14f13d5210c473220caf/completion"
+                val url = URL(urlString)
+                val connection = url.openConnection() as HttpURLConnection
+                connection.requestMethod = "POST"
+                connection.setRequestProperty("Content-Type", "application/json")
+                connection.doOutput = true
+
+                val postData = "{\"prompt\": \"$text\", \"n_predict\": 128}"
+                val outputStream = DataOutputStream(connection.outputStream)
+                outputStream.writeBytes(postData)
+                outputStream.flush()
+                outputStream.close()
+
+                val responseCode = connection.responseCode
+                if (responseCode == HttpURLConnection.HTTP_OK) {
+                    val reader = BufferedReader(InputStreamReader(connection.inputStream))
+                    val response = StringBuilder()
+                    var line: String?
+
+                    while (reader.readLine().also { line = it } != null) {
+                        response.append(line).append('\n')
+                    }
+                    reader.close()
+
+                    println("Réponse : ${response.toString()}")
+                } else {
+                    println("Erreur de requête : $responseCode")
+                }
+            } catch (e: Exception) {
+                println("Une exception s'est produite : ${e.message}")
+                e.printStackTrace()
+            }
         }
     }
 }
